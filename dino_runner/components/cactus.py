b83439cdc68bc995109788f9b8fdca42ce3e733d
miltonduquecano <mduquec8@eafit.edu.co>
2023-03-31 18:59:05 -0500

    Segunda Clase

---
+++ dino_runner/components/cactus.py
@@ -0,0 +1,11 @@
+import random
+from dino_runner.components.obstacle import Obstacle
+
+class Cactus(Obstacle):
+    # image es una lista
+    def __init__(self, image):
+        # self.type es como un indice
+        self.type = random.randint(0, 2)
+        super().__init__(image, self.type)
+        self.rect.y = 325
+        
---
+++ dino_runner/components/obstacle.py
@@ -0,0 +1,18 @@
+import pygame
+from pygame.sprite import Sprite
+from dino_runner.utils.constants import SCREEN_WIDTH
+
+class Obstacle(Sprite):
+    def __init__(self, image, obstacle_type):
+        # self.image es una lista
+        self.image = image
+        # self.obstacle_type es un indece
+        self.obstacle_type = obstacle_type
+        self.rect = self.image[self.obstacle_type].get_rect()
+        self.rect.x = SCREEN_WIDTH
+
+        def update(self, game_speed):
+            self.rect.x -= game_speed
+        def draw(self, screen):
+            screen.blit(self.image[self.obstacle_type], (self.rect.x, self.rect.y))
+
